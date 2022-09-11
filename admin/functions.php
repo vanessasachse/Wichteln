29cb68710be93f8c7e0527b5a862bbf2e1df3035
MilchreisMitZimt <github@scrnsht.eu>
2022-09-11 16:59:30 +0200

    prüffunktion

--- admin/functions.php
+++ admin/functions.php
@@ -10,21 +10,39 @@ function showerr($errmsg){
   exit();
 }
 
+function checkrolled(){
+  $rolled=0;
+  $res = selectsql("SELECT wichtel from zuweisungen");
+  $check=array();
+  while($row = $res->fetch_assoc()) {
+    $x = $row["wichtel"];
+    array_push($check, $x);
+  }
+
+  foreach ($check as &$value) {
+    if (!is_null($value)) {
+      $rolled=1;
+    }
+  }
+  return $rolled;
+}
+
 function selectsql($query){
   global $mysqli;
   $sql = $query;
   $res = $mysqli->query($sql);
   return $res;
 }
+
 function setwichtel($teilnehmer, $wichtel){
   global $mysqli;
   $sql = "UPDATE zuweisungen set wichtel='$wichtel' where teilnehmer='$teilnehmer'";
 
   if ($mysqli->query($sql) === TRUE) {
-  echo "$wichtel => $teilnehmer\n";
-} else {
-  echo "Error: " . $sql . "<br>" . $mysqli->error;
-}
+    echo "$wichtel => $teilnehmer\n";
+  } else {
+    echo "Error: " . $sql . "<br>" . $mysqli->error;
+  }
 
 }
 
