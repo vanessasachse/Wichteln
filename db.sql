e62f10d05e5e50eb042e4de9a97361ea282227f5
MilchreisMitZimt <mmz@gurkec>
2022-09-13 22:43:44 +0200

    update

--- db.sql
+++ db.sql
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `teilnehmer`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `teilnehmer` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
-  `code` varchar(8) NOT NULL,
+  `code` varchar(255) DEFAULT NULL,
   `dname` varchar(50) DEFAULT NULL,
   `wishlist` text DEFAULT NULL,
   `adresse` varchar(255) DEFAULT NULL,
@@ -32,7 +32,7 @@ CREATE TABLE `teilnehmer` (
   `favs` text DEFAULT NULL,
   `notlike` text DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,8 +41,32 @@ CREATE TABLE `teilnehmer` (
 
 LOCK TABLES `teilnehmer` WRITE;
 /*!40000 ALTER TABLE `teilnehmer` DISABLE KEYS */;
-INSERT INTO `teilnehmer` VALUES (1,'a','dominik','iftjasdio','ijfaiosdIJFIOASD','IJFAOISDJ','IOFJASDOI','IJA'),(2,'a','öüä','öüäöüäöü','öäü','öäüä','üäöüäöüäff','öäü'),(3,'test','öüä','öüäöüäöü','öäü','öäüä','asdf','öäü'),(4,'a','asdfasd','asdf','afsdf','asdafsdf','aa','asdfasd');
 /*!40000 ALTER TABLE `teilnehmer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `zuweisungen`
+--
+
+DROP TABLE IF EXISTS `zuweisungen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `zuweisungen` (
+  `teilnehmer` varchar(255) NOT NULL,
+  `wichtel` varchar(255) DEFAULT NULL,
+  `trackingid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`teilnehmer`),
+  UNIQUE KEY `wichtel` (`wichtel`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `zuweisungen`
+--
+
+LOCK TABLES `zuweisungen` WRITE;
+/*!40000 ALTER TABLE `zuweisungen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `zuweisungen` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -54,4 +78,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-10 21:09:12
+-- Dump completed on 2022-09-13 22:43:40
