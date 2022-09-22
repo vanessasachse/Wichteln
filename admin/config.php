e4a42011ac02491e07818df00547f69e0c440797
Dominik <mmz@gurkec>
2022-09-22 22:38:01 +0200

    weitere parameter hinzugefügt

--- admin/config.php
+++ admin/config.php
@@ -3,6 +3,9 @@ $DBHOST='gulasch.fritz.box';
 $DBUSER='wichteln';
 $DBPASS='blabla123';
 $DBNAME='wichteln';
+$REGCLOSED=0;
+$ROLLDATE="XX.XX.XXXX";
+$ROLLTIME="XX:XX";
 $ALLOWREROLL=false;
 $CODELENGTH=32;
 
